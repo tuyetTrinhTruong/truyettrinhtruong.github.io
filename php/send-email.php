f1addb383238e9d15cb94097a77ea5c45dce6591
thanhluan2706 <21211tt3881@mail.tdc.edu.vn>
2024-07-15 13:12:19 +0700

    update

--- php/send-email.php
+++ php/send-email.php
@@ -1,29 +1,32 @@
 <?php
 
 // Replace this with your own email address
-$to = 'joefrey.mahusay@gmail.com';
+$to = 'tuyettrinhtruong0322@gmail.com';
 
-function url(){
-  return sprintf(
-    "%s://%s",
-    isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off' ? 'https' : 'http',
-    $_SERVER['SERVER_NAME']
-  );
+function url()
+{
+   return sprintf(
+      "%s://%s",
+      isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off' ? 'https' : 'http',
+      $_SERVER['SERVER_NAME']
+   );
 }
 
-if($_POST) {
+if ($_POST) {
 
    $name = trim(stripslashes($_POST['name']));
    $email = trim(stripslashes($_POST['email']));
    $subject = trim(stripslashes($_POST['subject']));
    $contact_message = trim(stripslashes($_POST['message']));
 
-   
-	if ($subject == '') { $subject = "Contact Form Submission"; }
+
+   if ($subject == '') {
+      $subject = "Contact Form Submission";
+   }
 
    // Set Message
    $message .= "Email from: " . $name . "<br />";
-	 $message .= "Email address: " . $email . "<br />";
+   $message .= "Email address: " . $email . "<br />";
    $message .= "Message: <br />";
    $message .= nl2br($contact_message);
    $message .= "<br /> ----- <br /> This email was sent from your site " . url() . " contact form. <br />";
@@ -32,17 +35,18 @@ if($_POST) {
    $from =  $name . " <" . $email . ">";
 
    // Email Headers
-	$headers = "From: " . $from . "\r\n";
-	$headers .= "Reply-To: ". $email . "\r\n";
- 	$headers .= "MIME-Version: 1.0\r\n";
-	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+   $headers = "From: " . $from . "\r\n";
+   $headers .= "Reply-To: " . $email . "\r\n";
+   $headers .= "MIME-Version: 1.0\r\n";
+   $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 
    ini_set("sendmail_from", $to); // for windows server
    $mail = mail($to, $subject, $message, $headers);
 
-	if ($mail) { echo "OK"; }
-   else { echo "Something went wrong. Please try again."; }
-
+   if ($mail) {
+      echo "OK";
+   } else {
+      $errorMessage = error_get_last()['message'];
+       echo "Đã xảy ra lỗi. Vui lòng thử lại. Lỗi: " . $errorMessage;
+   }
 }
-
-?>
